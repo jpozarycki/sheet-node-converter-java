62a75c893284e9b9faa92276acf7584641c61ea8
jpozarycki <jakub.pozarycki@gmail.com>
2020-07-09 17:55:34 +0200

    added logic to add recursively nodes

--- src/main/java/com/jpozarycki/Main.java
+++ src/main/java/com/jpozarycki/Main.java
@@ -2,23 +2,25 @@ package com.jpozarycki;
 
 import com.jpozarycki.model.Node;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        XSSFWorkbook wb = getWorkbook("test.xlsx");
-        List<Node> nodes = getNodesFromWorkbook(wb);
+        XSSFWorkbook wb = getWorkbookFromResources("test.xlsx");
+        XSSFSheet mainSheet = wb.getSheetAt(0);
+        int lastLayer = mainSheet.getRow(0).getLastCellNum() - 2;
+        List<Node> nodes = createNodes(mainSheet, Collections.emptyList(), lastLayer);
+        System.out.println(nodes.toString());
     }
 
-    private static XSSFWorkbook getWorkbook(String fileName) {
+    private static XSSFWorkbook getWorkbookFromResources(String fileName) {
         try (InputStream is = Main.class.getClassLoader().getResourceAsStream(fileName)) {
             return new XSSFWorkbook(is);
         } catch (IOException e) {
@@ -26,18 +28,31 @@ public class Main {
         }
     }
 
-    private static List<Node> getNodesFromWorkbook(XSSFWorkbook workbook) {
-        XSSFSheet mainSheet = workbook.getSheetAt(0);
-        Iterator<Row> rows = mainSheet.rowIterator();
-        List<Node> nodes = new ArrayList<>();
-
-        while (rows.hasNext()) {
-            Row row = rows.next();
-            Iterator<Cell> cells = row.cellIterator();
-            while (cells.hasNext()) {
-                System.out.println(cells.next().toString());
+    private static List<Node> createNodes(XSSFSheet sheet, List<Node> nodes, Integer layer) {
+        if (layer < 0) {
+            return nodes;
+        }
+        List<Node> newNodes = new ArrayList<>();
+        int lastRowIdx = sheet.getLastRowNum();
+        for (int i = 1; i <= lastRowIdx; i++) {
+            Row row = sheet.getRow(i);
+            Cell cell = row.getCell(layer);
+            if (cell != null && cell.getCellType() != CellType.BLANK) {
+                int lastCellIdx = row.getLastCellNum() - 1;
+                Node newNode = new Node(lastCellIdx, cell.getStringCellValue());
+                newNodes.add(newNode);
             }
         }
-        return nodes;
+        for (Node childNode : nodes) {
+            for (Node parentNode : newNodes) {
+                if (childNode.getName().startsWith(parentNode.getName())) {
+                    List<Node> parentNodeNodes = parentNode.getNodes();
+                    parentNodeNodes.add(childNode);
+                    parentNode.setNodes(parentNodeNodes);
+                }
+            }
+        }
+
+        return createNodes(sheet, newNodes, layer - 1);
     }
 }
--- src/main/java/com/jpozarycki/model/Node.java
+++ src/main/java/com/jpozarycki/model/Node.java
@@ -1,9 +1,19 @@
 package com.jpozarycki.model;
 
+import lombok.Data;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Data
 public class Node {
-    Integer id;
-    String name;
-    List<Node> nodes;
+    private Integer id;
+    private String name;
+    private List<Node> nodes;
+
+    public Node (Integer id, String name) {
+        this.id = id;
+        this.name = name;
+        this.nodes = new ArrayList<>();
+    }
 }
